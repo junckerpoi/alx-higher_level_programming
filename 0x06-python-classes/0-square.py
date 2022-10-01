29ce6537016527185f947eda654ad3d5e95b7b14
junckerpoi <natifx901@gmail.com>
2022-09-30 22:32:04 -0700

    intial commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-"""Define a class square"""
-
-
 class Square:
-        """reapting a square"""
-            pass
+    pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
-"""Define a class square"""
-
-
 class Square:
-        """Represent a square."""
-
-            def __init__(self, size):
-                        """Initialize a new Square.
-                                Args:
-                                            size (int): The size of the new square.
-                                                    """
-                                                            self.__size = size
+    def __init__(self, size):
+        self.__size = size
