8402c31758e926ec99e2bafaf826c65374a7b209
junckerpoi <natifx901@gmail.com>
2022-10-15 02:19:31 -0700

    sympathy

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""creates python object from json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Args:
+        filename (file pointer): json file to load
+    """
+    with open(filename) as file:
+        return json.loads(file.read())
