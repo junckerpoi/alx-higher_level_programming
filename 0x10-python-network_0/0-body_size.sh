7ad53c8602c5bd3fde9d984b5921c8ad78b61f2a
junckerpoi <natifx901@gmail.com>
2024-03-17 05:52:03 -0700

    3rd

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '        
-
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
